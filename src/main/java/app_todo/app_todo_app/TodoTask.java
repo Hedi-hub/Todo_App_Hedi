fcd691cd7a6372a32c9786cef29f2a7578b71cba
Hedi-hub <72658290+Hedi-hub@users.noreply.github.com>
2023-06-01 15:29:07 +0200

    added table columns

--- src/main/java/app_todo/app_todo_app/TodoTask.java
+++ src/main/java/app_todo/app_todo_app/TodoTask.java
@@ -8,4 +8,44 @@ public class TodoTask {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column(name="title")
+    private String taskTitle;
+
+    @Column(name="status")
+    private boolean complete;
+
+    public TodoTask(){
+    }
+
+    public todoTask(String taskTitle, boolean complete){
+        this.taskTitle = taskTitle;
+        this.complete = complete;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+
 }
